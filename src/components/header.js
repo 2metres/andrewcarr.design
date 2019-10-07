e0e4cf8a12ce13eedce7b6e6ffb3cfee157aa5eb
Andrew Carr <andrew@2metres.com>
2019-10-07 23:57:37 +1100

    Remove unnecissary folders

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,7 @@ const Header = ({
     className="px-12 py-24"
   >
     <div className="container mx-auto">
-      <h1 className="text-3xl font-display font-black">
+      <h1 className="text-5xl font-display leading-none break-words uppercase mb-5 font-black">
         {title}
       </h1>
       <h3>
